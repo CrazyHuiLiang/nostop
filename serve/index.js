03d4c04080d8096731a7b1f2d93445511ecbeeee
huiliang.zhang <huiliang.zhang@jinshuschool.com>
2023-11-16 15:59:55 +0800

    完成 nostop serve

--- serve/index.js
+++ serve/index.js
@@ -8,25 +8,50 @@ const working_dir = process.cwd();
 const body_parser = require('./body_parser');
 
 // 浏览文件系统
-function explore(req, res) {
-    const dir_path = working_dir + req.url;
+async function explore(req, res) {
+    let param_entry;
+    if (/[&?]entry=[^&#]+/gi.test(req.url)) {
+        param_entry = decodeURIComponent(/[&?]entry=(?<entry>[^&#]+)/gi.exec(req.url).groups.entry);
+    }
+    const entry = working_dir + param_entry;
+    console.log('explore:', entry);
 
-    const index_html = fs.readFileSync(__dirname + '/index.html');
-    return res.end(index_html); // TODO
-
-    if (!fs.existsSync(dir_path)) {
+    if (!fs.existsSync(entry)) {
         res.statusCode = 404;
         return res.end('Not Found');
     }
-    const ent = fs.statSync(dir_path);
+    const ent = fs.statSync(entry);
     if (ent.isDirectory()) {
-        const nodes = fs.readdirSync(dir_path);
+        const dir = fs.opendirSync(entry);
+        const nodes = [];
+        if (param_entry && param_entry !== '/') {
+            nodes.push({
+                name: '..',
+                isDir: true,
+            });
+        }
+        for await (const dirent of dir) {
+            nodes.push({
+                name: dirent.name,
+                isDir: dirent.isDirectory(),
+            });
+        }
+        // log(nodes);
+        nodes.sort((node1, node2) => {
+            if (node1.isDir && !node2.isDir) {
+                return -1;
+            }
+            if (!node1.isDir && node2.isDir) {
+                return 1;
+            }
+            return 0;
+        });
         return res.end(JSON.stringify({
             data: nodes
         }), 'utf-8');
     }
     if (ent.isFile()) {
-        return res.end(fs.readFileSync(dir_path));
+        return res.end(fs.readFileSync(entry));
     }
     res.statusCode = 400;
     res.end('Unknown file type.');
@@ -35,9 +60,9 @@ function explore(req, res) {
 // 文件上传
 async function controller_upload(req, res) {
     const body_parsed = await body_parser.simpleParser(req);
-    let up_path = __dirname;
+    let up_path = working_dir;
     body_parsed.forEach(item => {
-        if (item.header_parsed.name === 'path') {
+        if (item.header_parsed.name === 'entry') {
             up_path += item.body.toString();
         }
     });
@@ -48,34 +73,46 @@ async function controller_upload(req, res) {
         }
     });
     log(up_path, body_parsed);
+    res.end(`{"success": true}`);
 }
+
 // 文件下载
 function controller_download(req, res) {
     log(req, res);
+    res.end(`{"success": false, "info": "not support."}`);
 }
 
 exports.start_serve = function (port = 3000) {
+
+    const inter = os.networkInterfaces();
+    let addresses = Object.values(inter).flat().filter(item => item.family === 'IPv4');
+    if (addresses.length > 1) {
+        addresses = addresses.filter(item => item.address !== '127.0.0.1');
+    }
+    let url;
+    addresses.forEach(item => {
+        url = item.address + ':' + port;
+    });
+
+
     const server = http.createServer((req, res) => {
+        console.log(req.url);
+        if (req.url.startsWith('/_s_explore')) {
+            return explore(req, res);
+        }
         if (req.url.startsWith('/_s_upload')) {
             return controller_upload(req, res);
         }
         if (req.url.startsWith('/_s_download')) {
             return controller_download(req, res);
         }
-        explore(req, res);
+        const index_html = fs.readFileSync(__dirname + '/index.html', "utf-8");
+        return res.end(index_html.replace('__ip__', url));
     });
     server.on('listening', () => {
-        const inter = os.networkInterfaces();
         console.log('server start at:');
-        addresses = Object.values(inter).flat().filter(item => item.family === 'IPv4');
-        if (addresses.length > 1) {
-            addresses = addresses.filter(item => item.address !== '127.0.0.1');
-        }
-        addresses.forEach(item => {
-            const url = 'http://' + item.address + ':' + port;
-            console.log(url);
-            qrcode.generate(url);
-        });
+        console.log('http://' + url);
+        qrcode.generate('http://' + url);
     });
     server.listen(port);
 }
